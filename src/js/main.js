9391287a095ee3abde8c6d2203f2d53096690404
Dmitrii Matveev <matveyeff@mail.ru>
2019-12-11 01:17:33 +0300

    added slider

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 400,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		cssEase: 'ease',
+  });
+});
