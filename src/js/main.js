bace60dbcb8682e228985b76e89dec3028c72173
Dmitrii Matveev <matveyeff@mail.ru>
2019-12-23 15:58:18 +0300

    added navigation for phones

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,23 @@ $("#nextContent").click(function(e){
 	$("#nextContentList").fadeIn();
 	$("#nextContent").hide();
 });
+
+// Hamburger Menu
+
+var burgerBtn = document.getElementById('burgerBtn');
+var open = document.getElementById('nav');
+var style = open.style;
+
+burgerBtn.addEventListener('click', function() {
+	style.display = 'flex';
+	burgerBtn.style.display = 'none';
+	close.style.display = 'flex';
+});
+
+var close = document.getElementById('close');
+
+close.addEventListener('click', function() {
+	close.style.display = 'none';
+	style.display = 'none';
+	burgerBtn.style.display = 'flex';
+});
